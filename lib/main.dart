8d9cd21852ba8b5d736ef25abb3759ad0d543841
Saksham Sahu <saksham.sahu.02052002@gmail.com>
2023-07-16 12:57:12 +0530

    unused imports removed

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter_catalog/Pages/home_page.dart';
 import 'package:flutter_catalog/Pages/login_page.dart';
 import 'package:flutter_catalog/Utils/routes.dart';
 import 'package:flutter_catalog/Widgets/themes.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
