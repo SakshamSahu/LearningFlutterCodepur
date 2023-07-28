b08c4200eb54d6c8a246dd93555da7c4e07159a2
Saksham Sahu <saksham.sahu.02052002@gmail.com>
2023-07-28 11:46:15 +0530

    Day 16 completed

---
+++ lib/Models/cart.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_catalog/Models/catalog.dart';
+
+class CartModel {
+  //catalog field
+  CatalogModel? _catalog;
+  // CartModel(this._catalog);
+
+  //collection of ID's: Store Id's of each item
+  final List<int> _itemIds = [];
+
+  //get catalog
+  CatalogModel get catalog => _catalog!;
+
+  //set catalog
+  set catalog(CatalogModel newcatalog) {
+    _catalog = newcatalog;
+  }
+
+  //get items in the cart
+  List<Item> get items =>
+      _itemIds.map((id) => _catalog!.getById(id)).whereType<Item>().toList();
+
+  //get total price
+  num get totalPrice =>
+      items.fold(0, (total, current) => total + current.price);
+
+  //add item
+  void add(Item item) {
+    _itemIds.add(item.id);
+  }
+
+  //remove item
+  void remove(Item item) {
+    _itemIds.remove(item.id);
+  }
+}
--- lib/Pages/cart_page.dart
+++ lib/Pages/cart_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/Widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class CartPage extends StatelessWidget {
@@ -8,10 +7,94 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: context.canvasColor,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: "Cart".text.make(),
+        backgroundColor: context.canvasColor,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: "Cart".text.make(),
+        ),
+        body: const Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(32),
+                child: _CartList(),
+              ),
+            ),
+            Divider(),
+            _CartTotal(),
+          ],
+        ));
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  const _CartTotal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text(
+            "\$9999",
+            style: TextStyle(
+              fontSize: 40.0,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+          ),
+          const WidthBox(30.0),
+          ElevatedButton(
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text(
+                    " Buying not supported yet. ",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              );
+            },
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Theme.of(context)
+                    .floatingActionButtonTheme
+                    .backgroundColor)),
+            child: const Text(
+              "Buy",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ).w32(context),
+        ],
+      ),
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  const _CartList({super.key});
+
+  @override
+  State<_CartList> createState() => _CartListState();
+}
+
+class _CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      // physics: const BouncingScrollPhysics(),
+      // shrinkWrap: true,
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+        leading: const Icon(Icons.done),
+        trailing: IconButton(
+          icon: const Icon(Icons.remove_circle_outline),
+          onPressed: () {},
+        ),
+        title: const Text("Item 1"),
       ),
     );
   }
